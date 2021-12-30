5c46a0a5c0b9e1677c873186511ac4a88b8bc86d
kperkins97 <perkins.kelsey@yahoo.com>
2021-12-30 14:33:46 -0600

    fixing server file

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
+const routes = require('./routes');
 // const { MongoClient: mongodb, ObjectId } = require('mongodb');
 // const connectionStringURI = `mongodb://localhost:27017/noSqlSocialDB`;
 
 const mongoose = require('mongoose');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noSqlSocialDB', {
     useFindAndModify: false,
@@ -17,9 +18,11 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noSqlSoci
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+// app.use(require('./routes'));
 
-app.use(require('./routes'));
-
-app.listen(PORT, () => {
-console.log(`API server running on port ${PORT}!`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
